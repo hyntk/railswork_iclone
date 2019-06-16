a128a576bdfefaaedb47562cd0087361ad1886ce
hyntk <hoge@hoge.com>
2019-06-16 23:27:11 +0900

    feat: sessionコントローラ及びviewを作成

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,12 @@
 Rails.application.routes.draw do
+  get 'sessions/new'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   resources:feeds do
     collection do
       post :confirm
     end
   end
+
+  resources :users, only: [:new, :create, :show]
+  resources :sessions, only: [:new, :create, :destroy]
 end
