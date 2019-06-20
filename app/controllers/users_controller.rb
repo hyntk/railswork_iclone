8954fef72b158c2d1b3bb96c61effd413caf0ba4
hyntk <hyntkjp@gmail.com>
2019-06-20 18:30:41 +0900

    feat:ユーザープロフィール編集機能の実装

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -24,7 +24,7 @@ class UsersController < ApplicationController
 
   def update
     @user = User.find(params[:id])
-    if @feed.update(user_params)
+    if @user.update(user_params)
       redirect_to user_path, notice: "ユーザーを編集しました！"
     else
       render 'edit'
--- config/routes.rb
+++ config/routes.rb
@@ -8,7 +8,7 @@ Rails.application.routes.draw do
     end
   end
 
-  resources :users, only: [:new, :create, :show, :edit]
+  resources :users, only: [:new, :create, :show, :edit, :update]
   resources :sessions, only: [:new, :create, :destroy]
 
   resources :favorites, only: [:create, :destroy]
